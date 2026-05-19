554287dc37b248574ae27a11f5ab6798327447a2
tulio <tulio89lopes@gmail.com>
2026-05-19 18:55:57 -0300

    feat(backend): implementa decorator e guard de roles

---
+++ backend/src/auth/guards/roles.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLE_KEY } from '../decorators/roles.decorator';
+import { Role } from '@prisma/client';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    const user = request.user;
+
+    return requiredRoles.includes(user.role);
+  }
+}
