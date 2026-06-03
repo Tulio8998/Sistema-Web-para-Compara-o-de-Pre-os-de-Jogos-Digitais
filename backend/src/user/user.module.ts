410801989ff85bebd94a3af5d764c534ad3efc00
tulio <tulio89lopes@gmail.com>
2026-06-03 16:56:41 -0300

    feat(backend): importa Service de GameAPi em modulo de user comentarios

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { GameApiService } from 'src/game-api/game-api.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, GameApiService],
   exports: [UserService],
 })
 export class UserModule {}
