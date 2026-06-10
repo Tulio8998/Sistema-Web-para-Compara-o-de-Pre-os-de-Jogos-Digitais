ee33e70b9a88b1f995f3f49121b08dfcf5d39bd9
tulio <tulio89lopes@gmail.com>
2026-06-10 16:08:01 -0300

    remove(backend): remove verificacao de roles em controller de game-api

--- backend/src/game-api/game-api.controller.ts
+++ backend/src/game-api/game-api.controller.ts
@@ -8,8 +8,6 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { GameApiService } from './game-api.service';
-import { Roles } from 'src/auth/decorators/roles.decorator';
-import { Role } from '@prisma/client';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 
@@ -19,19 +17,16 @@ export class GameApiController {
   constructor(private readonly gameService: GameApiService) {}
 
   @Get('search/:title')
-  @Roles(Role.ADMIN, Role.CLIENT)
   searchGameByTitle(@Param('title') title: string) {
     return this.gameService.searchGameByTitle(title);
   }
 
   @Get('info/:id')
-  @Roles(Role.ADMIN, Role.CLIENT)
   getGameInfo(@Param('id') id: string) {
     return this.gameService.getGameInfo(id);
   }
 
   @Get('deals')
-  @Roles(Role.ADMIN, Role.CLIENT)
   getDeals(
     @Query('limit') limit?: number,
     @Query('offset') offset?: number,
@@ -42,19 +37,16 @@ export class GameApiController {
   }
 
   @Post('prices')
-  @Roles(Role.ADMIN, Role.CLIENT)
   getGamePrices(@Body() gameIds: string[], @Query('country') country?: string) {
     return this.gameService.getGamePrices(gameIds, country);
   }
 
   @Post('history-low')
-  @Roles(Role.ADMIN, Role.CLIENT)
   getHistoryLow(@Body() gameIds: string[], @Query('country') country?: string) {
     return this.gameService.getHistoryLow(gameIds, country);
   }
 
   @Post('store-low')
-  @Roles(Role.ADMIN, Role.CLIENT)
   getStoreLow(
     @Body() gameIds: string[],
     @Query('country') country?: string,
@@ -64,13 +56,11 @@ export class GameApiController {
   }
 
   @Get('history-log/:id')
-  @Roles(Role.ADMIN, Role.CLIENT)
   getHistoryLog(@Param('id') id: string, @Query('country') country?: string) {
     return this.gameService.getHistoryLog(id, country);
   }
 
   @Post('overview')
-  @Roles(Role.ADMIN, Role.CLIENT)
   getOverview(@Body() gameIds: string[], @Query('country') country?: string) {
     return this.gameService.getOverview(gameIds, country);
   }
